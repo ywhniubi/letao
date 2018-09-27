ce82d7d0b557017916a8cc53e196458d3ddb9b97
beer.ywh <778652070@qq.com>
2018-09-27 20:54:11 +0800

    首页模态框完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,11 +1,51 @@
 
 
 
-console.log(2)
 
+//进度条功能
 $(document).ajaxStart(function(){
 	NProgress.start();
 })
 $(document).ajaxStop(function(){
 	NProgress.done();
+})
+
+
+
+
+// 1.二级菜单切换
+$(function(){
+	$(" .catrioy .category").click(function(){
+
+		$(" .catrioy  .left_child").stop().slideToggle();
+	})
+
+
+
+
+
+})
+
+
+
+//2.左侧菜单栏切换
+$(function(){
+	$(".menu-left").click(function(){
+		$(".lt_leftside").css("left","-180px")
+		$(".lt_leftside").css("transition","1s")
+		$(".lt_rightside").css("padding-left","0px")
+		$(".lt_rightside").css("transition","1s")
+		$(".right_top").css("padding-left","10px")
+		$(".right_top").css("transition","1s")
+	})
+
+
+		$(".exit-right").click(function(){
+
+			$("#model").modal("toggle")
+
+
+
+		})
+
 })
