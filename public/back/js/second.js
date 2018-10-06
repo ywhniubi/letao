77de25097d8eed6ba3815e8299a5d05965268f62
beer.ywh <778652070@qq.com>
2018-10-06 12:20:22 +0800

    第二分类完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -55,8 +55,7 @@ $.ajax({
 $(".add_btn").on("click",function(){
 
 $("#add_model").modal("show");
-
-$.ajax({
+	$.ajax({
 	type:"get",
 	url:"/category/queryTopCategoryPaging",
 	data:{
@@ -65,7 +64,7 @@ $.ajax({
 	},
 	success:function(info){
 		var htmltxt=template("tmp2",info);
-		$(".slide_menu").html(htmltxt);
+		$(".dropdown-menu").html(htmltxt);
 
 	}
 	
@@ -79,27 +78,22 @@ $.ajax({
 
 })
 
-$("#slide_btn").on("click",function(){
-	console.log(1)
-
-$(".slide_menu").toggle("hide")
-})
 
 
+$(".dropdown-menu").on("click","a",function(){
 
-$(".slide_menu").on("click","a",function(){
 	var txt = $(this).text();
 	$("#dropdownTxt").text(txt);
 	 // 获取当前 a 中存储的 id
     var id = $(this).data("id");
     // 设置给 name="categoryId" 的input
-    $('[name="categoryId"]').val( id );
+    $('[name="categoryId"]').val(id);
+
+  $('#form').data("bootstrapValidator").updateStatus("categoryId", "VALID");
 
 
+  });
 
-
-
-})
 
 $("#fileupload").fileupload({
 	dataType:"json",
@@ -109,12 +103,15 @@ $("#fileupload").fileupload({
 		console.log(data.result)
 		//获取图片地址
 		var picurl=data.result.picAddr;
-
+		console.log(picurl)
 		$('#imgBox img').attr("src", picurl);//将图片地址赋值给图片盒子让其显示
 		 // 将图片地址设置给 name="brandLogo" 的 input 用于提交
-      $('[name="brandLogo"]').val( picUrl );
+      $('[name="brandLogo"]').val(picurl);
 
 
+     
+         // 重置校验状态
+      $('#form').data("bootstrapValidator").updateStatus("brandLogo", "VALID");
 	}
 
 })
@@ -122,8 +119,81 @@ $("#fileupload").fileupload({
 
 
 
+  // 5. 进行表单校验初始化
+  $("#form").bootstrapValidator({
+    // 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)']
+    // 需要对隐藏域进行校验, 不能排除隐藏域, 将 excluded 置为 [], 表示对所有 input 进行校验
+    excluded: [],
 
 
+    // 指定校验时显示的图标, 固定写法
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',      // 校验成功
+      invalid: 'glyphicon glyphicon-remove',   // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+
+    // 配置校验字段
+    fields: {
+        categoryId: {
+          validators: {
+            notEmpty: {
+              message: "请选择一级分类"
+            }
+          }
+        },
+       brandName: {
+         validators: {
+           notEmpty: {
+             message: "请输入二级分类"
+           }
+         }
+       },
+       brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请选择图片"
+          }
+        }
+      }
+    }
+  });
+
+//6. 注册表单校验成功事件, 阻止默认的表单提交, 通过 ajax 提交
+  $('#form').on("success.form.bv", function( e ) {
+    console.log(1)
+
+    e.preventDefault();
+
+  console.log(2)
+    // 通过 ajax 提交
+    $.ajax({
+      type: "post",
+      url: "/category/addSecondCategory",
+      data: $('#form').serialize(),
+      dataType: "json",
+      success: function( info ) {
+        console.log( info );
+        if ( info.success ) {
+          alert(1)
+          // 关闭模态框
+          $('#addModal').modal("hide");
+
+          // 页面需要重新渲染, 重新渲染第一页
+          currentPage = 1;
+          render();
+
+          // 表单需要重置, 文本和状态都要重置
+          $('#form').data("bootstrapValidator").resetForm(true);
+
+          // 重置只能重置表单元素, 下拉菜单的按钮和图片需要手动重置
+          $('#dropdownTxt').text("请选择一级分类");
+          $('#imgBox img').attr("src", "images/none.png");
+        }
+      }
+    })
+
+  })
 
 
 
